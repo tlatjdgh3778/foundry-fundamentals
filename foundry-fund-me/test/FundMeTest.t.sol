59464dae8e56260eb13f543e23ec4774e88ec17b
tjdgh3778 <tjdgh3778@gmail.com>
2025-06-15 23:04:20 +0900

    ✨ FundMeTest 계약에 가격 피드 버전 확인 테스트 추가: 가격 피드 버전이 정확한지 검증하는 새로운 테스트 함수 구현

--- foundry-fund-me/test/FundMeTest.t.sol
+++ foundry-fund-me/test/FundMeTest.t.sol
@@ -19,4 +19,20 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public view {
         assertEq(fundMe.i_owner(), address(this));
     }
+
+    // What can we do to work with addresses outside our system ?
+    // 1. Unit
+    //     - Testing a specific part of our code
+    // 2. Integration
+    //     - Testing how our code works with other parts of our code
+    // 3. Forked
+    //     - Testing our code on a simulated real environment
+    // 4. Staging
+    //     - Testing our code in a real environment that is not prod
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        console.log("version", version);
+        assertEq(version, 4);
+    }
 }
